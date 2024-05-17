cde94f41ab464865764669dea5696bdb733a528d
Smoren <ofigate@gmail.com>
2024-05-17 10:22:46 +0300

    Printer added.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <map>
 #include "math/math.h"
+#include "printer/printer.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     math::NumericVector<int> v1 = {1, 2, 3};
     math::NumericVector<int> v2 = {10, 20, 30};
     math::NumericVector<int> v3 = {11, 22, 33};
+    std::cout << (v1 + v2) << std::endl;
     std::cout << (v1 + v2).abs() << std::endl;
 
     std::map<math::NumericVector<int>, int> map = {{v1, 111}, {v2, 222}, {v3, 333}};
---
+++ printer/printer.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <tuple>
+#include <utility>
+#include <set>
+#include <map>
+#include <deque>
+
+template <typename Collection>
+std::string join(const Collection& collection, const std::string& delimiter);
+
+template <typename First, typename Second>
+std::ostream& operator <<(std::ostream& stream, const std::pair<First, Second>& p);
+
+template <typename Key, typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::map<Key, Value>& m);
+
+template <typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::vector<Value>& m);
+
+template <typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::deque<Value>& m);
+
+template <typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::set<Value>& m);
+
+
+template <typename Collection>
+std::string join(const Collection& collection, const std::string& delimiter) {
+    bool isFirst = true;
+    std::stringstream ss;
+    for(auto& item : collection) {
+        if(!isFirst) {
+            ss << delimiter;
+        } else {
+            isFirst = false;
+        }
+        ss << item;
+    }
+    return ss.str();
+}
+
+template <typename First, typename Second>
+std::ostream& operator <<(std::ostream& stream, const std::pair<First, Second>& p) {
+    // требует подключения utility
+    return stream << p.first << ": " << p.second;
+}
+
+template <typename Key, typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::map<Key, Value>& m) {
+    return stream << "{" << join(m, ", ") << "}";
+}
+
+template <typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::vector<Value>& m) {
+    return stream << "[" << join(m, ", ") << "]";
+}
+
+template <typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::set<Value>& m) {
+    return stream << "(" << join(m, ", ") << ")";
+}
+
+template <typename Value>
+std::ostream& operator <<(std::ostream& stream, const std::deque<Value>& m) {
+    return stream << "[" << join(m, ", ") << "]";
+}
